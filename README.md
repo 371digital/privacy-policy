b8340d8cb0080fb9c7fb6bd29f3f97904a6a9cb7
Gron <ahesteaheste0101@email.com>
2022-07-06 20:17:10 +0300

    Has been finished photoshoot privacy policy

---
+++ README.md
@@ -0,0 +1,3 @@
+### 371 Digital Privacy Policy Publish Service
+
+371 Digital bünyesinde ortaya çıkan ürünlerin privacy policy servisi için kullanılır.
---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/photoshoot', function (req, res) {
+    res.sendFile(path.join(__dirname + '/static/photoshoot.html'));
+});
+
+app.use(router);
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+console.log(`PRIVACY POLICY server start at http://localhost:${port}`)
